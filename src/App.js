3ab955dd53999816cf2dfebc034ad4bbbb44a0b6
Liam Callaghan <ljcallaghan64@gmail.com>
2021-04-07 19:05:44 +0100

    image shown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>Hello World</h1>
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Museum from './component/museum/Museum'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={Museum}/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/component/museum/Museum.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Museum extends React.Component {
+  state = {
+  }
+
+  render() {
+    return (
+      <>
+        <div>Museum</div>
+        <img src="https://ids.si.edu/ids/deliveryService?id=NPG-S_NPG_82_43Lincoln-000001" height="800" width="600" />
+      </>
+    )
+  }
+}
+
+export default Museum
